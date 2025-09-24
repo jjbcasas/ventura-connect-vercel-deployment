aabecbbbfb8a0887666a657621fa21474b1083e4
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-09-23 23:16:00 -0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -105,16 +105,6 @@ app.use('/api/feed', feedRoutes)
 app.use('/api/profile', profileRoutes)
 app.use('/api/post', postRoutes)
 
-// Serve static assets if in production
-if ( process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
-
-    app.get('/*path', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
-    })
-}
-
 // server running
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}, you better catch it`)
